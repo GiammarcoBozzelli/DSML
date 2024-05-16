b1abcd1f70f6fd1985812b3af529eb59fa9e67f0
Kurthhenry <55870958+Kurthhenry@users.noreply.github.com>
2024-05-16 15:31:54 +0200

    Add files via upload

---
+++ Code/DSML_Assignment_LR.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 14:30:51 2024
+
+@author: timbaettig
+"""
+
+"""
+DS & ML
+"""
+#install packages
+#!pip install -U spacy
+
+#imoprt packages
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import nltk
+nltk.download('stopwords')
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+
+#import data
+directory = "/Users/timbaettig/Library/Mobile Documents/com~apple~CloudDocs/00_Privat/00_EPFL/Courses/SS 2024/Data Science and Machine Learning/Project/Data/"
+training = pd.read_csv(directory+"training_data.csv")
+test = pd.read_csv(directory+"unlabelled_test_data.csv")
+
+#data exploration
+
+#data preparation
+def preprocess_text(text):
+    # Remove punctuation, digits, etc.
+    text = ''.join([char for char in text if char.isalpha() or char.isspace()])
+    # Lowercase
+    text = text.lower()
+    # Tokenization and remove stop words
+    tokens = text.split()
+    stop_words = set(stopwords.words('french'))
+    tokens = [word for word in tokens if word not in stop_words]
+    # Stemming
+    stemmer = SnowballStemmer('french')
+    tokens = [stemmer.stem(word) for word in tokens]
+    return ' '.join(tokens)
+
+training['processed_sentence'] = training['sentence'].apply(preprocess_text)
+
+# Vectorize the training data
+vectorizer = TfidfVectorizer(max_features=1000)
+X_train = vectorizer.fit_transform(training['processed_sentence'])
+y_train = training['difficulty']
+
+# Train Logistic Regression model
+logistic_model = LogisticRegression(random_state=42, max_iter=1000)
+logistic_model.fit(X_train, y_train)
+
+#------------------------------------------------------------------------------------------------
+#Prediction Preparation 
+test['processed_sentence'] = test['sentence'].apply(preprocess_text)
+
+# Vectorize the test data
+X_test = vectorizer.transform(test['processed_sentence'])
+
+# Make predictions
+predicted_difficulties = logistic_model.predict(X_test)
+
+# Create a submission DataFrame
+submission = pd.DataFrame({
+    'id': test['id'],
+    'difficulty': predicted_difficulties
+})
+
+# Export to CSV
+submission.to_csv(directory+'Outputs/prediction1_log_reg.csv', index=False)
