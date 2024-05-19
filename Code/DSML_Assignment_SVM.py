28a08b06bb84dc6aefe31733863c6e3a61cac5fd
Kurthhenry <55870958+Kurthhenry@users.noreply.github.com>
2024-05-19 11:03:41 +0200

    Update DSML_Assignment_SVM.py

--- Code/DSML_Assignment_SVM.py
+++ Code/DSML_Assignment_SVM.py
@@ -27,9 +27,9 @@ from sklearn.svm import SVC
 
 
 #import data
-directory = "/Users/timbaettig/Library/Mobile Documents/com~apple~CloudDocs/00_Privat/00_EPFL/Courses/SS 2024/Data Science and Machine Learning/Project/Data/"
-training = pd.read_csv(directory+"training_data.csv")
-test = pd.read_csv(directory+"unlabelled_test_data.csv")
+#directory = "/Users/timbaettig/Library/Mobile Documents/com~apple~CloudDocs/00_Privat/00_EPFL/Courses/SS 2024/Data Science and Machine Learning/Project/Data/"
+training = pd.read_csv("https://raw.githubusercontent.com/GiammarcoBozzelli/DSML/main/DATA/training_data.csv")
+test = pd.read_csv("https://raw.githubusercontent.com/GiammarcoBozzelli/DSML/main/DATA/unlabelled_test_data.csv")
 
 #data exploration
 
@@ -79,4 +79,4 @@ submission = pd.DataFrame({
 })
 
 # Export to CSV
-submission.to_csv(directory+'Outputs/prediction3_log_reg_svm.csv', index=False)
+#submission.to_csv(directory+'Outputs/prediction3_log_reg_svm.csv', index=False)
