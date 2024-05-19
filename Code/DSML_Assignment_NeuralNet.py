ba7e8d3dc94bf21f65fe7ae3b1a76744782a59eb
Kurthhenry <55870958+Kurthhenry@users.noreply.github.com>
2024-05-19 20:24:10 +0200

    Update DSML_Assignment_NeuralNet.py

--- Code/DSML_Assignment_NeuralNet.py
+++ Code/DSML_Assignment_NeuralNet.py
@@ -11,40 +11,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import joblib
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from kerastuner.tuners import BayesianOptimization
 from keras_tuner import RandomSearch
 from sklearn.preprocessing import LabelEncoder
 
-#directory = "/Users/timbaettig/Library/Mobile Documents/com~apple~CloudDocs/00_Privat/00_EPFL/Courses/SS 2024/Data Science and Machine Learning/Project/Data/"
+directory = "/Users/timbaettig/Library/Mobile Documents/com~apple~CloudDocs/00_Privat/00_EPFL/Courses/SS 2024/Data Science and Machine Learning/Project/Data/"
 training = pd.read_csv("https://raw.githubusercontent.com/GiammarcoBozzelli/DSML/main/DATA/training_data.csv")
 test = pd.read_csv("https://raw.githubusercontent.com/GiammarcoBozzelli/DSML/main/DATA/unlabelled_test_data.csv")
 
 # DATA PROCESSING
 # ------------------------------------------------------------------------------------------------
 def preprocess_text(text):
+    # Remove punctuation, digits, etc.
     text = ''.join([char for char in text if char.isalpha() or char.isspace()])
+    # Lowercase
     text = text.lower()
-    return text
+    # Tokenization and remove stop words
+    tokens = text.split()
+    stop_words = set(stopwords.words('french'))
+    tokens = [word for word in tokens if word not in stop_words]
+    # Stemming
+    stemmer = SnowballStemmer('french')
+    tokens = [stemmer.stem(word) for word in tokens]
+    return ' '.join(tokens)
 
 # Apply preprocessing
 training['processed_sentence'] = training['sentence'].apply(preprocess_text)
-test['processed_sentence'] = test['sentence'].apply(preprocess_text)
 
 # Tokenization and sequence padding
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(training['processed_sentence'])
-X_train = tokenizer.texts_to_sequences(training['processed_sentence'])
-X_test = tokenizer.texts_to_sequences(test['processed_sentence'])
+X = tokenizer.texts_to_sequences(training['processed_sentence'])
 
 # Pad sequences to ensure uniform input size
-X_train = pad_sequences(X_train, maxlen=75)
-X_test = pad_sequences(X_test, maxlen=75)
+X = pad_sequences(X, maxlen=75)
 
 # Checking unique difficulty levels
 unique_classes = training['difficulty'].unique()
@@ -55,8 +66,11 @@ print("Number of classes:", num_classes)
 
 # Convert labels to one-hot encoding
 encoder = LabelEncoder()
-y_train = encoder.fit_transform(training['difficulty'])
-y_train = to_categorical(y_train, num_classes=num_classes)
+y = encoder.fit_transform(training['difficulty'])
+y = to_categorical(y, num_classes=num_classes)
+
+#Train Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 # BUILDING THE NEURAL NET
@@ -76,28 +90,56 @@ def build_model(hp):
     return model
 
 
-
 # HYPERPARAMETER TUNING
 # ------------------------------------------------------------------------------------------------
-tuner = RandomSearch(
+tuner = BayesianOptimization(
     build_model,
     objective='val_accuracy',
     max_trials=5,
-    executions_per_trial=1,
+    executions_per_trial=2,
     directory='my_dir',
-    project_name='keras_tuning'
+    project_name='keras_tuning',
+    overwrite=True
 )
 
-tuner.search(X_train, y_train, epochs=10, validation_split=0.2)
+tuner.search(X_train, y_train, epochs=5, validation_split=0.2)
 
-best_model = tuner.get_best_models(num_models=1)[0]
-
-predictions = best_model.predict(X_test)
+best_nn_model = tuner.get_best_models(num_models=1)[0]
 
 
-# RESULTS
+# Save the trained SVM model
+nn_model_path = directory + "nn_model.pkl"
+joblib.dump(best_nn_model, nn_model_path)
+
+# Evalutaion
 # ------------------------------------------------------------------------------------------------
-predicted_classes = np.argmax(predictions, axis=1)  # Get the class index with the highest probability
+# Predict and evaluate
+y_test = np.argmax(y_test, axis=1)
+y_test = encoder.inverse_transform(y_test)
+
+y_pred = best_nn_model.predict(X_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+y_pred_labels = encoder.inverse_transform(y_pred_classes)
+
+accuracy = accuracy_score(y_test, y_pred_labels)
+conf_matrix = confusion_matrix(y_test, y_pred_labels)
+report = classification_report(y_test, y_pred_labels, target_names=['A1', 'A2', 'B1', 'B2', 'C1', 'C2'])
+
+# Print results
+print(f"Accuracy: {accuracy}")
+print("Confusion Matrix:")
+print(conf_matrix)
+print("Classification Report:")
+print(report)
+
+
+# Prediction
+# ------------------------------------------------------------------------------------------------
+test['processed_sentence'] = test['sentence'].apply(preprocess_text)
+X_test_final = tokenizer.texts_to_sequences(test['processed_sentence'])
+X_test_final = pad_sequences(X_test_final, maxlen=75)
+predictions = best_nn_model.predict(X_test_final)
+predicted_classes = np.argmax(predictions, axis=1)
 
 # Convert class indices back to original labels
 predicted_labels = encoder.inverse_transform(predicted_classes)
