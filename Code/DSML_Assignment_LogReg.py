97024b364c3b34c5c2d2288a607f391c2e77500e
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2024-05-23 22:26:34 +0200

    Update DSML_Assignment_LogReg.py

--- Code/DSML_Assignment_LogReg.py
+++ Code/DSML_Assignment_LogReg.py
@@ -24,8 +24,8 @@ from sklearn.linear_model import LogisticRegression
 
 #import data
 #directory = "/Users/timbaettig/Library/Mobile Documents/com~apple~CloudDocs/00_Privat/00_EPFL/Courses/SS 2024/Data Science and Machine Learning/Project/Data/"
-training = pd.read_csv("https://raw.githubusercontent.com/GiammarcoBozzelli/DSML/main/DATA/training_data.csv")
-test = pd.read_csv("https://raw.githubusercontent.com/GiammarcoBozzelli/DSML/main/DATA/unlabelled_test_data.csv")
+training = pd.read_csv("https://raw.githubusercontent.com/Kurthhenry/DSML/main/DATA/training_data.csv")
+test = pd.read_csv("https://raw.githubusercontent.com/Kurthhenry/DSML/main/DATA/unlabelled_test_data.csv")
 
 #data preparation
 def preprocess_text(text):
